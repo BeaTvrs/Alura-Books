38490928290815d0524d23cee3bc254d8cb8c6c9
Beatriz Tavares <biamottatavares@gmail.com>
2025-09-03 10:30:41 -0300

    adiciona função para ordenar livros por preço e inclui script no HTML

---
+++ app/metodoSort.js
@@ -0,0 +1,7 @@
+let bntOrdenarPorPreco = document.getElementById ('btnOrdenarPorPreco');
+bntOrdenarPorPreco.addEventListener('click', ordenarLivrosPorPreco);
+
+function ordenarLivrosPorPreco () {
+  let livrosOrdenados = livros.sort ((a, b) => a.preco - b.preco);
+  exibirOsLivrosNaTela (livrosOrdenados);
+}
